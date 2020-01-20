a369bc860125c39d63db4ca9a109d32b8c548909
Kathrin Schuler <kathrinschuler5@gmail.com>
2020-01-20 21:36:28 +0000

    Update database schema

--- models/schema.py
+++ models/schema.py
@@ -1,24 +1,45 @@
 import sqlite3
 
 
-class Schema:
-    def __init__(self):
-        self.conn = sqlite3.connect('easystudy.db')
-        self.create_lists_table()
-        self.create_words_table()
+def setup_tables():
+    conn = sqlite3.connect('easystudy.db')
+    create_lists_table(conn)
+    create_words_table(conn)
+    create_mapping_table(conn)
 
-    def create_lists_table(self):
-        pass
 
-    def create_words_table(self):
-        query = """
-        CREATE TABLE IF NOT EXISTS "words" (
-          original TEXT,
-          translation TEXT,
-          sentence TEXT,
-          CONSTRAINT original_translation PRIMARY KEY (original, translation)
-        );
-        """
+def create_lists_table(conn):
+    query = """
+    CREATE TABLE IF NOT EXISTS "lists" (
+      id TEXT PRIMARY KEY,
+      title TEXT,
+    );
+    """
+    conn.execute(query)
 
-        self.conn.execute(query)
 
+def create_words_table(conn):
+    query = """
+    CREATE TABLE IF NOT EXISTS "words" (
+      id TEXT PRIMARY KEY,
+      original TEXT NOT NULL,
+      translation TEXT NOT NULL,
+      sentence_original TEXT,
+      sentence_translation TEXT,
+    );
+    """
+
+    conn.execute(query)
+
+
+def create_mapping_table(conn):
+    query = """
+    CREATE TABLE IF NOT EXISTS "word_to_list" (
+        word_id TEXT,
+        list_id TEXT,
+        CONSTRAINT word_list PRIMARY KEY (word_id, list_id),
+        FOREIGN KEY(word_id) REFERENCES words(id),
+        FOREIGN KEY(list_id) REFERENCES lists(id)
+    );
+    """
+    conn.execute(query)
