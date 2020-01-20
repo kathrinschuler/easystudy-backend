b4beb76ce9d38ff0f960d83218f0e8ceb19f0a40
Kathrin Schuler <kathrinschuler5@gmail.com>
2020-01-20 21:17:11 +0000

    Flesh out API

--- app.py
+++ app.py
@@ -5,24 +5,45 @@ from service.service import EasyStudyService
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/lists", methods=["GET"])
+def get_all_lists():
+    pass
 
 
-@app.route("/<name>")
-def hello_name(name):
-    return "Hello " + name
+@app.route("/lists", methods=["POST"])
+def create_new_list():
+    pass
 
 
-@app.route("/word", methods=["POST"])
-def add_word():
-    return EasyStudyService().add_word(request.get_json())
+@app.route("/lists/<list_id>", methods=["GET"])
+def get_list(list_id):
+    pass
 
 
-@app.route("/word", methods=["GET"])
-def list_words():
-    return jsonify(EasyStudyService().list())
+@app.route("/lists/<list_id>", methods=["DELETE"])
+def delete_list(list_id):
+    pass
+
+
+@app.route("/lists/<list_id>", methods=["POST"])
+def update_list(list_id):
+    pass
+
+
+@app.route("/lists/<list_id>/words", methods=["GET"])
+def get_words_for_list(list_id):
+    pass
+    # return EasyStudyService().add_word(request.get_json())
+
+
+@app.route("/lists/<list_id>/words", methods=["POST"])
+def add_word_to_list(list_id):
+    pass
+
+
+@app.route("/lists/<list_id>/words/<word_id>", methods=["DELETE"])
+def delete_word_from_list(list_id):
+    pass
 
 
 if __name__ == "__main__":
